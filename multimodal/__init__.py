4cae5e0e388ec74ed7aa8cb1e00ac95507b8ecf9
Tasio Mendez <tasio.mendez.ayerbe@alumnos.upm.es>
2019-06-05 16:42:13 +0200

    Download nltk libs if not already downloaded

---
+++ multimodal/__init__.py
@@ -0,0 +1,21 @@
+import nltk
+
+try:
+    nltk.data.find('corpora/opinion_lexicon')
+except LookupError:
+    nltk.download('opinion_lexicon')
+
+try:
+    nltk.data.find('corpora/wordnet')
+except LookupError:
+    nltk.download('wordnet')
+
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
